7fc1945656fbbc251824cd044bb2e68781dae7b2
Peter Huber <peterhub@peters-macbook-pro.local>
2015-12-21 18:40:36 -0500

    Initial Commit

---
+++ ImpulseInterpreter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ImpulseInterpreter
+//
+//  Created by Peter Huber on 2015-12-21.
+//  Copyright © 2015 Huberis Technologies. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
